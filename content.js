53ca3a8ace2f623b43a37a71318819744dd121c6
sayedpritom999 <sayedpritom999@gmail.com>
2020-10-26 00:03:58 +0600

    commit

--- content.js
+++ content.js
@@ -1,13 +1,6 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    // const num = parseInt(request.message);
-    // if(num) {
-    //   // sessionStorage.setItem('time', num);
-    //   // location.reload()
-    // }
-    // const time, minTime, maxTime;
     if((request.message).length == 2) {
-      // time = parseInt(request.message);
       if(request.message[1] == "setCustomTime") {
         sessionStorage.setItem('minTime', null);
         sessionStorage.setItem('maxTime', null);
@@ -15,50 +8,43 @@ chrome.runtime.onMessage.addListener(
       sessionStorage.setItem("time", request.message[0]);
       location.reload()
     }
-    else {
+    else if((request.message).length == 3) {
       if(request.message[2] == "randomInput") {
         sessionStorage.setItem('time', null);
       }
       sessionStorage.setItem("minTime", request.message[0]);
       sessionStorage.setItem("maxTime", request.message[1]);
       location.reload()
+    } else {
+        sessionStorage.setItem('minTime', null);
+        sessionStorage.setItem('maxTime', null);
+        sessionStorage.setItem('time', null);
+        location.reload()
     }
-    console.log(request.message);    
-    // sessionStorage.setItem(request.message);
-// start(num);
+    // console.log(request.message);    
     }
 );
 
-// function start(num){
-//     // alert(num);
-//     console.log(num);
-//     location.reload()
-// }
 let timeInput = parseInt(sessionStorage.getItem("time"));
 let randomTime = () => {
   let minTime = parseInt(sessionStorage.getItem("minTime"));
   let maxTime = parseInt(sessionStorage.getItem("maxTime"));
   const diff = maxTime - minTime;
   const time = (Math.random()*diff)+parseInt(minTime);
-  // console.log(time)
   return time;
 }
 const randomTime2 = randomTime();
 
 
 if(timeInput) {
-      // countdown(timeInput);
       setInterval(() => {
-          // popup(timeInput*1000)
-          console.log(timeInput);
+          // console.log(timeInput);
+        location.reload();
       }, (timeInput*1000));
   }
   else if(randomTime2) {
-      // setInterval(() => {
-      //   // popup(randomTime2*1000)
-      // }, (randomTime2*1000));
       setTimeout(() => {        
-        console.log(randomTime2);
+        // console.log(randomTime2);
         location.reload();
       }, (randomTime2*1000));
   }
--- popup.js
+++ popup.js
@@ -1,31 +1,3 @@
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        // console.log(minutes);
-        seconds = parseInt(timer % 60, 10);
-        
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-        
-        display.textContent = minutes + ":" + seconds;
-        
-        if (--timer < 0) {
-            console.log("now");
-            // popup(1)
-            timer = duration;
-        }
-    }, 1000);
-}
-
-const countdown = time => {
-    window.onload = function () {
-        var fiveMinutes = time,
-            display = document.querySelector('#time');
-        startTimer(fiveMinutes, display);
-    }
-};
-
 const getElement = id => document.getElementById(id);
 const reload = time => setInterval(() => {
     location.reload();
@@ -37,72 +9,30 @@ const minutesFromButtons = document.getElementsByClassName("min");
 for (let i = 0; i < minutesFromButtons.length; i++) {
     const time = minutesFromButtons[i];
     time.addEventListener("click", () => {
-        setTimeValue((time.innerHTML)*60);
-        sessionStorage.setItem('minTime', null);
-        sessionStorage.setItem('maxTime', null);
+        popup([((time.innerHTML)*60), "setCustomTime"])
         location.reload();
     })    
 }
-
-const randomInput = document.getElementById("randomInputForm");
-const setCustomTime = document.getElementById("setCustomTime");    
+   
+const randomInput = getElement("randomInputForm");
+const setCustomTime = getElement("setCustomTime");    
 
 setCustomTime.addEventListener('submit', (e) => {
     e.preventDefault();
-    const time = document.getElementById('custom-seconds').value;
-    // sessionStorage.setItem('time', time);
+    const time = getElement('custom-seconds').value;
     popup([time, "setCustomTime"])
     location.reload();
 })
 
 randomInput.addEventListener('submit', (e) => {
       e.preventDefault();
-      const min = document.getElementById('from').value;
-      const max = document.getElementById('to').value;
-
-    //   sessionStorage.setItem('minTime', min);
-    //   sessionStorage.setItem('maxTime', max);
+      const min = getElement('from').value;
+      const max = getElement('to').value;
       popup([min,max, "randomInput"])
       location.reload();
     })
-
-// let timeInput = parseInt(sessionStorage.getItem("time"));
-// let randomTime = () => {
-//     let minTime = parseInt(sessionStorage.getItem("minTime"));
-//     let maxTime = parseInt(sessionStorage.getItem("maxTime"));
-//     const diff = maxTime - minTime;
-//     const time = (Math.random()*diff)+parseInt(minTime);
-//     console.log(time)
-//     return time;
-// }
-// const randomTime2 = randomTime();
-// if(timeInput) {
-//     countdown(timeInput);
-//     setInterval(() => {
-//         // popup(timeInput*1000)
-//     }, (timeInput*1000));
-// }
-// else if(randomTime2) {
-//     setInterval(() => {
-//         location.reload();
-//         popup(randomTime2*1000)
-//         console.log(randomTime2);
-//     }, (randomTime2*1000));
-// }
-
 document.getElementById("stopBtn").addEventListener('click', () => {
-    sessionStorage.setItem('minTime', null);
-    sessionStorage.setItem('maxTime', null);
-    sessionStorage.setItem('time', null)
-    location.reload();
-    });
-document.getElementById("randomBtn").addEventListener('click', () => {
-    sessionStorage.setItem('time', null);
-    location.reload();
-    });
-document.getElementById("customTimeBtn").addEventListener('click', () => {
-    sessionStorage.setItem('minTime', null);
-    sessionStorage.setItem('maxTime', null);
+    popup(["stopBtn"])
     location.reload();
     });
 
